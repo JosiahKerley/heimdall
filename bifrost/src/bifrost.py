13497b85cc40885f7de490099b4a8e6b0a580bc4
Josiah Kerley <josiahkerley@gmail.com>
2015-03-04 22:34:58 -0700

    Added initial nginx configurator

---
+++ bifrost/src/bifrost.py
@@ -0,0 +1,61 @@
+#import nginxparser as nginx
+
+settings = {
+	"nginxroot":"/etc/nginx",
+	"nginxconf":"/etc/nginx/conf.d",
+}
+
+backends = [
+	{
+		"name":None,
+		"url":None,
+		"enable":False,
+		"whitelist":[None],
+		"blacklist":[None],
+	},
+]
+
+
+server = {
+	"host":"$http_host",
+	"port":"80",
+	"ssl":"",
+	"resolver":"127.0.0.1",
+	"location":"/",
+	"scheme":"$scheme",
+	"host":"$host",
+	"uri":"$request_uri",
+	"auth":'Restricted',
+	"authfile":'%s/.htpasswd'%(settings['nginxroot']),
+}
+
+
+
+configtemplate = '''
+server {
+ resolver <[resolver]>;
+ access_log off;
+ listen  [::]:<[port]> <[ssl]>;
+ location <[location]> {
+   auth_basic '<[auth]>';
+   auth_basic_user_file '<[authfile]>';
+   proxy_pass <[scheme]>://<[host]><[uri]>;
+   proxy_set_header Host <[host]>;
+   proxy_buffers 256 4k;
+   proxy_max_temp_file_size 0k;
+ }
+}'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
